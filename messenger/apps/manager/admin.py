832ae2b197b00adb9a5ea6d3a439843f41ef3a39
juanargoti <juaargoti2015@hotmail.com>
2019-10-04 20:02:40 -0500

    basecon un error

--- messenger/apps/manager/admin.py
+++ messenger/apps/manager/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
+# importa todos los modelos 
+admin.site.register(category)
+admin.site.register(author)
+admin.site.register(post)
+admin.site.register(web)
+admin.site.register(social)
+admin.site.register(contact)
+admin.site.register(suscribers)
+
+
+
+
+
 
 # Register your models here.
+
+
--- messenger/apps/manager/models.py
+++ messenger/apps/manager/models.py
@@ -10,8 +10,8 @@ class main(models.Model):
     modify_date = models.DateField('creation date',auto_now=True,auto_now_add=False)
     delete_date =models.DateField('Delete date ',auto_now=True, auto_now_add=False)
 
-    class Meta:
-        abstract = True 
+class Meta:
+    abstract = True 
 
 class category(main):    
     name =models.CharField('Category name',max_length=100, unique=True)
@@ -31,7 +31,7 @@ class author(main):
     facebook = models.URLField('Facebook',null = True,blank=True)
     twitter  = models.URLField('Twitter',null = True,blank=True)
     instagram  = models.URLField('Instagram',null = True,blank=True)
-    web  = models.URLField('web',null = True,blank=True)
+    webp = models.URLField('web',null = True,blank=True)
     class Meta:
         verbose_name = 'Author'
         verbose_name_plural = 'Authors'
@@ -56,6 +56,49 @@ class post(main):
     def __str__(self):
         return self.title
 
+class web(main):
+        about_us = models.TextField('About us ')
+        phone = models.EmailField('phone ',max_length=20)
+        email = models.CharField('E-mail',max_length=150)
+        address = models.CharField('Adress ',max_length=200)
+        class Meta:
+            verbose_name= 'Web'
+            verbose_name_plural = 'Webs' 
+        def __str__ (self):
+            return self.about_us
 
-    
-    
+
+
+class social (main):
+        facebook = models.URLField('Facebook',null = True,blank=True)
+        twitter  = models.URLField('Twitter',null = True,blank=True)
+        instagram  = models.URLField('Instagram',null = True,blank=True)
+        webp  = models.URLField('web',null = True,blank=True)
+        class Meta:
+            verbose_name = 'Social network'
+            verbose_name_plural = 'Social networks'
+        
+        def __str__(self):
+            return self.facebook
+
+class contact (main):
+        firts_name =models.CharField('Firts name',max_length=100)
+        last_name =models.CharField('Last name',max_length=100)
+        Email = models.EmailField('Email',max_length=150)
+        subject = models.CharField('Subject',max_length=100)
+        message = models.TextField('Message')
+        class Meta:
+            verbose_name = ('Contac')
+            verbose_name_plural = ('Contacs')
+        def __str__ (self):
+            return self.subject
+
+class suscribers (main):       
+        Email = models.EmailField('Email',max_length=150)        
+        class Meta:
+            verbose_name = ('Suscriber')
+            verbose_name_plural = ('Suscribers')
+        def __str__ (self):
+            return self.Email          
+
+
